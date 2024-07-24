c7d560585c5a0db7c1bcfe80d98156202a4bfe44
Dedekind561 <mitchell.lloyd154@gmail.com>
2024-07-24 14:26:09 +0100

    prettify files

--- src/app/components/EventList.jsx
+++ src/app/components/EventList.jsx
@@ -1,20 +1,21 @@
 import EventCard from './EventCard.jsx';
 
 async function fetchEvents() {
-  return fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=${process.env.TICKETMASTER_API_KEY}&keyword=jazz&city=Manchester`) // Returns a promise, await async does this under the hood
-    .then(function(response) {
+  return fetch(
+    `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${process.env.TICKETMASTER_API_KEY}&keyword=jazz&city=Manchester`,
+  ) // Returns a promise, await async does this under the hood
+    .then(function (response) {
       return response.json();
     })
-    .then(function(jsonObject) {
-      // console.log(jsonObject._embedded.events,'<-- events'); 
-      // console.log(jsonObject._embedded.events.pop()._embedded.venues.pop(),'<-- venue'); 
-      return jsonObject._embedded.events
-    })
+    .then(function (jsonObject) {
+      // console.log(jsonObject._embedded.events,'<-- events');
+      // console.log(jsonObject._embedded.events.pop()._embedded.venues.pop(),'<-- venue');
+      return jsonObject._embedded.events;
+    });
 }
 
 export default async function EventList() {
-
-  const events_ = await fetchEvents()
+  const events_ = await fetchEvents();
 
   let events = events_.map((item) => {
     let venue = item._embedded.venues.pop();
@@ -26,12 +27,14 @@ export default async function EventList() {
       time: item.dates.start.localTime,
       location: venue && venue.city.name,
       venue: venue && venue.name,
-      url: item.url
-    }
-  })
+      url: item.url,
+    };
+  });
   return (
-      <div className="event-list">
-          {events.map(event => <EventCard key={event.id} {...event} />)}
-      </div>
-  )
+    <div className="event-list">
+      {events.map((event) => (
+        <EventCard key={event.id} {...event} />
+      ))}
+    </div>
+  );
 }
