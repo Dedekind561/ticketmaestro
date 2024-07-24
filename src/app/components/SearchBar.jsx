7872038e44d98dfcf347b0fa9e63abd74dc66664
Dedekind561 <mitchell.lloyd154@gmail.com>
2024-07-24 14:40:53 +0100

    update query string with user form input

--- src/app/components/SearchBar.jsx
+++ src/app/components/SearchBar.jsx
@@ -2,9 +2,23 @@
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function SearchBar() {
+  const searchParams = useSearchParams();
+  // A client component hook that lets you read the current URL's search parameters
+  const pathname = usePathname();
+  // A client component hook that lets you read the current URL's pathname.
+  const { replace } = useRouter();
+  // navigate to the provided href. Replaces the current history entry.
+
   function handleSubmit(formData) {
-    console.log('MMMM');
-    console.log(formData);
+    const query = formData.get('query');
+    // grab query string from form input
+    const params = new URLSearchParams(searchParams);
+    // create new instance of URLSearchParams
+
+    params.set('q', query);
+    // set the url search params
+    replace(`${pathname}?${params.toString()}`);
+    // navigate to the url with the provided query string
   }
 
   return (
