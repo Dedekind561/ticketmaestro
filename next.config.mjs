a451f50112ee2497b0339a1444b924116f8c6117
MitchLloyd <mitchell@ada.ac.uk>
2024-09-19 16:48:57 +0000

    change base path

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV === 'development') {
   nextConfig = {};
 } else {
   nextConfig = {
-    basePath: '/ticketmaestro',
+    basePath: '/',
     reactStrictMode: true,
   };
 }
